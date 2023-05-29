44a64ea9f27be4f35f55861af1e75f81629fc3c8
nmayol <02mayolneus@gmail.com>
2023-05-29 13:31:53 +0200

    no funciona

--- QP/Prac6/squares.pl
+++ QP/Prac6/squares.pl
@@ -12,6 +12,70 @@ ejemplo(5,112,[50,42,37,35,33,29,27,25,24,19,18,17,16,15,11,9,8,7,6,4,2]).
 ejemplo(6,175,[81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]).
 
 
+
+
+main:- 
+    ejemplo(1,Big,Sides),
+    nl, write('Fitting all squares of size '), write(Sides), write(' into big square of size '), write(Big), nl,nl,
+    length(Sides,N), 
+    length(RowVars,N), % get list of N prolog vars: Row coordinates of each small square
+    length(ColVars,N), % get list of N prolog vars: Column coordinates of each small square
+
+
+    % rowVars/colsVars conte les coordenades origen: aquelles que corresponen al canto superior esquerre
+
+    insideBigSquare(N,Big,Sides,RowVars), % cert si els quadrats es troba dins els limits.
+    insideBigSquare(N,Big,Sides,ColVars),
+    nonoverlapping(N,Sides,RowVars,ColVars),
+    append(RowVars,ColVars,Vars),
+    labeling([ff],Vars),
+    displaySol(Big,Sides,RowVars,ColVars), halt.
+
+insideBigSquare(N,B,[X|S],[Y|V]):-
+    N1 is N+1,
+    sum([X,Y], #<, N1), 
+    sum([X,Y], #>, 0), 
+insideBigSquare(N,B,S,V).
+    
+
+overlaps2(R1,C1,S1,R2,C2):-
+    R1 #< R2,
+    Rf is R1 + S1,
+    R2 #< Rf,
+
+    C1 #< C2,
+    Cf is C1 + C1,
+    C2 #< Cf.
+
+
+
+notOverlaps(_,_,_,[],[],[]).
+notOverlaps(R1,C1,S1,[S2|S],[R2|R],[C2|C]):-
+    not(overlaps2(R1,C1,S1,R2,C2)),
+    not(overlaps2(R1,C1,S1,R2+S2,C2+S2)),
+notOverlaps(R1,C1,S1,S,R,C).    
+
+
+
+nonoverlapping(_,[],[],[]).
+nonoverlapping(N,[X|S],[Y|RV],[Z|CV]):-
+    notOverlaps(Y,Z,X,S,RV,CV),
+nonoverlapping(N,S,RV,CV).
+
+
+
+displaySol(N,Sides,RowVars,ColVars):- 
+    between(1,N,Row), nl, between(1,N,Col),
+    nth1(K,Sides,S),    
+    nth1(K,RowVars,RV),    RVS is RV+S-1,     between(RV,RVS,Row),
+    nth1(K,ColVars,CV),    CVS is CV+S-1,     between(CV,CVS,Col),
+    writeSide(S), fail.
+displaySol(_,_,_,_):- nl,nl,!.
+
+writeSide(S):- S<10, write('  '),write(S),!.
+writeSide(S):-       write(' ' ),write(S),!.
+
+
 %% Possible output solution for example 3:
 %%  10 10 10 10 10 10 10 10 10 10  9  9  9  9  9  9  9  9  9
 %%  10 10 10 10 10 10 10 10 10 10  9  9  9  9  9  9  9  9  9
@@ -32,29 +96,3 @@ ejemplo(6,175,[81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]
 %%   3  3  3  3  3  3  4  4  4  4  3  3  3  3  3  3  3  3  3
 %%   3  3  3  3  3  3  4  4  4  4  3  3  3  3  3  3  3  3  3
 %%   3  3  3  3  3  3  1  1  1  1  3  3  3  3  3  3  3  3  3
-
-
-main:- 
-    ejemplo(3,Big,Sides),
-    nl, write('Fitting all squares of size '), write(Sides), write(' into big square of size '), write(Big), nl,nl,
-    length(Sides,N), 
-    length(RowVars,N), % get list of N prolog vars: Row coordinates of each small square
-    ...
-    insideBigSquare(N,Big,Sides,RowVars),
-    insideBigSquare(N,Big,Sides,ColVars),
-    nonoverlapping(N,Sides,RowVars,ColVars),
-    ...
-    displaySol(Big,Sides,RowVars,ColVars), halt.
-
-
-displaySol(N,Sides,RowVars,ColVars):- 
-    between(1,N,Row), nl, between(1,N,Col),
-    nth1(K,Sides,S),    
-    nth1(K,RowVars,RV),    RVS is RV+S-1,     between(RV,RVS,Row),
-    nth1(K,ColVars,CV),    CVS is CV+S-1,     between(CV,CVS,Col),
-    writeSide(S), fail.
-displaySol(_,_,_,_):- nl,nl,!.
-
-writeSide(S):- S<10, write('  '),write(S),!.
-writeSide(S):-       write(' ' ),write(S),!.
-
